f1ef0ca464b852081a3c8a974261b2f815ecfb50
GwangCheonLee <raitmid56@gmail.com>
2024-07-20 23:34:32 +0900

    refactor: change Column GenerationType uuid

--- src/main/java/com/getreadyforthenext/core/user/entities/User.java
+++ src/main/java/com/getreadyforthenext/core/user/entities/User.java
@@ -1,30 +1,35 @@
 package com.getreadyforthenext.core.user.entities;
 
+import com.getreadyforthenext.core.user.enums.AuthenticationProvider;
 import com.getreadyforthenext.core.user.enums.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
+@NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
 
     @Email(message = "Email should be valid")
     @Column(nullable = false, unique = true)
@@ -38,9 +43,8 @@ public class User {
     @Column(nullable = false)
     private String name;
 
-    @NotBlank(message = "Name cannot be empty")
-    @Column(nullable = false)
-    private String authenticationProvider;
+    @Enumerated(EnumType.STRING)
+    private AuthenticationProvider authenticationProvider;
 
     @Enumerated(EnumType.STRING)
     @ColumnDefault("'USER'")
