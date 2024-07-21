75c356a0f653f67c22097fead15f82a3c180fe98
GwangCheonLee <raitmid56@gmail.com>
2024-07-21 14:23:40 +0900

    feat: Add CommonUtil class file

---
+++ src/main/java/com/getreadyforthenext/core/common/utils/CommonUtil.java
@@ -0,0 +1,22 @@
+package com.getreadyforthenext.core.common.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+
+@Component
+public class CommonUtil {
+
+    public String generatePassword(int length) {
+        final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_+=<>?";
+        final SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int position = random.nextInt(CHARACTERS.length());
+            password.append(CHARACTERS.charAt(position));
+        }
+
+        return password.toString();
+    }
+}
